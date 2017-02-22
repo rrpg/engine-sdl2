56f829e396131d572a5258cd5e0b2385ab4f7322
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-02-22 21:08:18 +0000

    the map array will depend on a width and height instead of being fixed

--- src/Map.hpp
+++ src/Map.hpp
@@ -7,7 +7,9 @@
 
 class Map {
 	private:
-	uint8_t m_aGrid[2500];
+	unsigned int m_iWidth = 0;
+	unsigned int m_iHeight = 0;
+	uint8_t *m_aGrid = 0;
 	std::vector<Actor*> m_vActors = {};
 
 	public:
