9929100a510acaad07e01e856df56a1404f5d631
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-02-26 11:51:45 +0000

    tileset parsed in the map

--- src/Map.cpp
+++ src/Map.cpp
@@ -2,8 +2,13 @@
 
 #include <string.h>
 #include <fstream>
+#include <libgen.h>
+#include "SDL2_framework/TextureManager.h"
+#include "SDL2_framework/Game.h"
 
 const int MAX_CHARS_PER_LINE = 1024;
+const int MAX_CHAR_TILESET_NAME = 100;
+const int MAX_CHAR_TILESET_FILE = 100;
 
 S_MapParsingResult Map::setMap(const char* mapFile) {
 	std::ifstream fin;
@@ -13,6 +18,8 @@ S_MapParsingResult Map::setMap(const char* mapFile) {
 		return ERROR_OPENING_FILE;
 	}
 
+
+	char *mapDir = dirname(const_cast<char*>(mapFile));
 	while (!fin.eof()) {
 		char buf[MAX_CHARS_PER_LINE];
 		// @check too long lines
@@ -22,14 +29,14 @@ S_MapParsingResult Map::setMap(const char* mapFile) {
 			continue;
 		}
 
-		_parseLine(buf);
+		_parseLine(mapDir, buf);
 	}
 
 	fin.close();
 	return OK;
 }
 
-int Map::_parseLine(const char *line) {
+int Map::_parseLine(const char *mapDir, const char *line) {
 	int retValue = OK;
 
 	if (strlen(line) < 3) {
@@ -51,11 +58,49 @@ int Map::_parseLine(const char *line) {
 				++line;
 			}
 			break;
+		case 't':
+			retValue = _parseTileset(mapDir, line);
+			break;
 		default:
 			break;
 	}
 
 	return retValue;
+}
+
+S_MapParsingResult Map::_parseTileset(const char *mapDir, const char *line) {
+	Tileset tileset;
+	char tilesetPath[MAX_CHAR_TILESET_FILE],
+		 tilesetName[MAX_CHAR_TILESET_NAME];
+	unsigned int tilesetWidth, tileSize;
+	// format is:
+	// tilesetname filepath tilesize tilesetwidth
+	int res = sscanf(
+		line,
+		"%s %s %u %u",
+		tilesetName, tilesetPath, &tileSize, &tilesetWidth
+	);
+
+	if (res != 4) {
+		return INVALID_TILESET_DEFINITION;
+	}
+
+	m_tileset.name = tilesetName;
+	std::string textureFile = std::string(mapDir) + "/" + tilesetPath;
+
+	// first add the tileset to texture manager
+	TextureManager::Instance()->load(
+		textureFile,
+		m_tileset.name,
+		Game::Instance()->getRenderer()
+	);
+
+	m_tileset.tileWidth = tileSize;
+	m_tileset.tileHeight = tileSize;
+	m_tileset.width = tileSize * tilesetWidth;
+	m_tileset.name = tilesetName;
+	m_tileset.numColumns = tilesetWidth;
+	return OK;
 }
 
 void Map::addActor(Actor *actor) {
--- src/Map.hpp
+++ src/Map.hpp
@@ -4,22 +4,26 @@
 #include "Actor.hpp"
 #include <stdint.h>
 #include <vector>
+#include "SDL2_framework/Level.h"
 
 enum S_MapParsingResult {
 	OK,
 	ERROR_OPENING_FILE,
 	INVALID_LINE_FORMAT,
-	INVALID_DIMENSIONS_FORMAT
+	INVALID_DIMENSIONS_FORMAT,
+	INVALID_TILESET_DEFINITION
 };
 
 class Map {
 	private:
 	unsigned int m_iWidth = 0;
 	unsigned int m_iHeight = 0;
+	Tileset m_tileset;
 	std::vector<char> m_vGrid = {};
 	std::vector<Actor*> m_vActors = {};
 
-	int _parseLine(const char *line);
+	int _parseLine(const char *mapDir, const char *line);
+	S_MapParsingResult _parseTileset(const char *mapDir, const char *line);
 
 	public:
 	S_MapParsingResult setMap(const char* mapFile);
