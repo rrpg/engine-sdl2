49120c1e99e89b954d365252763dcf971d9abc03
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-02-26 18:35:49 +0000

    tile information added in the actor class, must be moved though

--- src/Actor.cpp
+++ src/Actor.cpp
@@ -4,3 +4,8 @@ void Actor::setX(int x) { m_iX = x; }
 void Actor::setY(int y) { m_iY = y; }
 int Actor::getX() { return m_iX; }
 int Actor::getY() { return m_iY; }
+
+int Actor::getFrame() { return m_iFrame; }
+void Actor::setFrame(int frame) { m_iFrame = frame; }
+int Actor::getTilesetRowIndex() { return m_iTilesetRowIndex; }
+void Actor::setTilesetRowIndex(int tilesetRowIndex) { m_iTilesetRowIndex = tilesetRowIndex; }
--- src/Actor.hpp
+++ src/Actor.hpp
@@ -5,12 +5,19 @@ class Actor {
 	private:
 	int m_iX = 0;
 	int m_iY = 0;
+	// @TODO move in TileRenderer class to create
+	int m_iFrame = 0;
+	int m_iTilesetRowIndex = 0;
 
 	public:
 	void setX(int x);
 	void setY(int y);
 	int getX();
 	int getY();
+	int getFrame();
+	void setFrame(int frame);
+	int getTilesetRowIndex();
+	void setTilesetRowIndex(int tilesetRowIndex);
 };
 
 #endif
