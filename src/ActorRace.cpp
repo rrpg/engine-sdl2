e1aa7b87946a78b87a4e297bab4d4bda30203c02
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-03-08 23:23:12 +0000

    the race now has a tileset and a sprite coordinate in the tileset

--- src/ActorRace.cpp
+++ src/ActorRace.cpp
@@ -9,3 +9,15 @@ Actor* ActorRace::createActor() {
 	actor->setMaxHealth(m_iLevel1HP);
 	return actor;
 }
+
+void ActorRace::setTilesetName(std::string name) {
+	m_sTilesetName = name;
+}
+
+void ActorRace::setSpriteX(unsigned int spriteX) {
+	m_iSpriteX = spriteX;
+}
+
+void ActorRace::setSpriteY(unsigned int spriteY) {
+	m_iSpriteY = spriteY;
+}
--- src/TaxonomyParser.cpp
+++ src/TaxonomyParser.cpp
@@ -1,8 +1,10 @@
 #include "TaxonomyParser.hpp"
 #include "ActorRace.hpp"
+#include "SDL2_framework/TextureManager.h"
+#include "SDL2_framework/Game.h"
 #include <iostream>
 
-const int MAX_CHAR_RACE_NAME = 100;
+const int MAX_CHAR_FILE_PATH = 255;
 
 TaxonomyParser::TaxonomyParser(ActorFactory &actorFactory) : m_actorFactory(actorFactory) {
 }
@@ -10,13 +12,28 @@ TaxonomyParser::TaxonomyParser(ActorFactory &actorFactory) : m_actorFactory(acto
 bool TaxonomyParser::_parseLine(const char *fileDir, const char *line) {
 	std::cout << fileDir << std::endl;
 	bool retValue = true;
-	unsigned int raceLevel1HP;
-	int result = sscanf(line, "%u\n", &raceLevel1HP);
-	if (result != 1) {
+	char tilesetPath[MAX_CHAR_FILE_PATH];
+	unsigned int raceLevel1HP, spriteX, spriteY;
+	int result = sscanf(
+		line,
+		"%s %u %u %u\n",
+		tilesetPath, &spriteX, &spriteY, &raceLevel1HP
+	);
+	if (result != 4) {
 		retValue = false;
 	}
 	else {
+		std::cout << std::string(fileDir) + "/" + tilesetPath << std::endl;
+		TextureManager::Instance()->load(
+			std::string(fileDir) + "/" + tilesetPath,
+			tilesetPath,
+			Game::Instance()->getRenderer()
+		);
+
 		ActorRace* race = new ActorRace(raceLevel1HP);
+		race->setTilesetName(tilesetPath);
+		race->setSpriteX(spriteX);
+		race->setSpriteY(spriteY);
 		m_actorFactory.addActorRaceTaxonomy(race);
 	}
 
