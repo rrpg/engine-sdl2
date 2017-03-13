c442badc02706a9df79d921494f558901289d21c
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-03-13 13:01:08 +0000

    method added to know if an actor sees another (uses Bresenham's line algorithm)

--- src/BehaviourMonster.cpp
+++ src/BehaviourMonster.cpp
@@ -2,10 +2,62 @@
 #include "Command/MoveDown.hpp"
 #include "rRpg.hpp"
 #include <iostream>
+#include <math.h>
+
+#define LIMIT_MONSTER_FIELD_OF_VIEW 6
 
 bool BehaviourMonster::update(rRpg *engine, Actor *actor) {
 	bool updated = true;
-	MoveDownCommand c = MoveDownCommand();
-	c.execute(actor, engine->getMap());
+	if (_sees(engine->getMap(), actor, engine->getHero())) {
+		std::cout << actor << " sees player\n";
+	}
 	return updated;
+}
+
+bool BehaviourMonster::_sees(Map &map, Actor *actor1, Actor *actor2) {
+	int x0, y0, x1, y1, deltaX, deltaY, directionX, directionY;
+	x0 = actor1->getX();
+	y0 = actor1->getY();
+	x1 = actor2->getX();
+	y1 = actor2->getY();
+	deltaX = x1 - x0;
+	deltaY = abs(y1 - y0);
+
+	directionX = x0 > x1 ? -1 : 1;
+	directionY = y0 > y1 ? -1 : 1;
+
+	// actors are verticals
+	if (deltaX == 0) {
+		for (int y = y0 + directionY; y != y1; y += directionY) {
+			if (map.isCellObstructingView(x0, y)) {
+				return false;
+			}
+		}
+	}
+	else if (deltaY == 0) {
+		for (int x = x0 + directionX; x != x1; x += directionX) {
+			if (map.isCellObstructingView(x, y0)) {
+				return false;
+			}
+		}
+	}
+	// diagonale
+	else if (abs(deltaX) == abs(deltaY)) {
+		int x = x0 + directionX;
+		for (int y = y0 + directionY; x != x1; x += directionX, y += directionY) {
+			if (map.isCellObstructingView(x, y)) {
+				return false;
+			}
+		}
+	}
+	// steep slope
+	else if (deltaX < deltaY) {
+		return false;
+	}
+	// gentle slope
+	else if (deltaX > deltaY) {
+		return false;
+	}
+
+	return true;
 }
--- src/BehaviourMonster.hpp
+++ src/BehaviourMonster.hpp
@@ -2,8 +2,12 @@
 #define __BEHAVIOUR_MONSTER__
 
 #include "Behaviour.hpp"
+#include "Map.hpp"
 
 class BehaviourMonster : public Behaviour {
+	private:
+	bool _sees(Map &map, Actor *actor1, Actor *actor2);
+
 	public:
 	bool update(rRpg *engine, Actor *actor);
 };
