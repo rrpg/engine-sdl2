7fddb3d2467308d22e06395e0a07667d7bb4cd23
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-03-14 12:00:39 +0000

    ifs refactored

--- src/BehaviourMonster.cpp
+++ src/BehaviourMonster.cpp
@@ -15,7 +15,10 @@ bool BehaviourMonster::update(rRpg *engine, Actor *actor) {
 }
 
 bool BehaviourMonster::_sees(Map &map, Actor *actor1, Actor *actor2) {
-	int x0, y0, x1, y1, deltaX, deltaY, absDeltaX, absDeltaY, directionX, directionY;
+	int x0, y0, x1, y1, x, y,
+		deltaX, deltaY, absDeltaX, absDeltaY,
+		directionX, directionY;
+	float slope;
 	x0 = actor1->getX();
 	y0 = actor1->getY();
 	x1 = actor2->getX();
@@ -28,47 +31,24 @@ bool BehaviourMonster::_sees(Map &map, Actor *actor1, Actor *actor2) {
 	directionX = x0 > x1 ? -1 : 1;
 	directionY = y0 > y1 ? -1 : 1;
 
-	// actors are verticals
-	if (deltaX == 0) {
-		for (int y = y0 + directionY; y != y1; y += directionY) {
-			if (map.isCellObstructingView(x0, y)) {
-				return false;
-			}
-		}
+	slope = 0;
+	if (deltaX != 0) {
+		slope = deltaY / (float) deltaX;
 	}
-	else if (deltaY == 0) {
-		for (int x = x0 + directionX; x != x1; x += directionX) {
-			if (map.isCellObstructingView(x, y0)) {
-				return false;
-			}
-		}
-	}
-	// diagonale
-	else if (absDeltaX == absDeltaY) {
-		int x = x0 + directionX;
-		for (int y = y0 + directionY; x != x1; x += directionX, y += directionY) {
+
+	// actors are vertical or on a steep slope
+	if (absDeltaX < absDeltaY) {
+		for (y = y0 + directionY; y != y1; y += directionY) {
+			x = deltaX == 0 ? x0 : round((y - y0 + slope * x0) / slope);
 			if (map.isCellObstructingView(x, y)) {
 				return false;
 			}
 		}
 	}
-	// steep slope
-	else if (absDeltaX < absDeltaY) {
-		float a = deltaY / (float) deltaX;
-		float x;
-		for (int y = y0 + directionY; y != y1; y += directionY) {
-			x = (y - y0 + a * x0) / a;
-			if (map.isCellObstructingView(x, round(y))) {
-				return false;
-			}
-		}
-	}
-	// gentle slope
-	else if (absDeltaX > absDeltaY) {
-		float a = deltaY / (float) deltaX;
-		float y;
-		for (int x = x0 + directionX; x != x1; x += directionX) {
-			y = round(a * x + y0 - a * x0);
+	// actors are horizonal or on a gentle slope
+	else {
+		for (x = x0 + directionX; x != x1; x += directionX) {
+			y = round(slope * x + y0 - slope * x0);
 			if (map.isCellObstructingView(x, y)) {
 				return false;
 			}
