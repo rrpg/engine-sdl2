1bd1dfa90c12686fe78b85032fc87f826b56e3dc
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-03-14 13:40:22 +0000

    if the monster sees the player, he moves towards him

--- src/BehaviourMonster.cpp
+++ src/BehaviourMonster.cpp
@@ -1,14 +1,54 @@
 #include "BehaviourMonster.hpp"
+#include "Command/MoveUp.hpp"
 #include "Command/MoveDown.hpp"
+#include "Command/MoveLeft.hpp"
+#include "Command/MoveRight.hpp"
 #include "rRpg.hpp"
-#include <iostream>
 #include <math.h>
+#include <algorithm>
 
 bool BehaviourMonster::update(rRpg *engine, Actor *actor) {
 	bool updated = true;
-	if (actor->seesActor(engine->getMap(), engine->getHero())) {
-		std::cout << actor << " sees player\n";
+	if (!actor->isNextTo(engine->getHero())
+		&& actor->seesActor(engine->getMap(), engine->getHero())
+	) {
+		_executeMove(
+			engine,
+			actor,
+			engine->getHero()->getX() - actor->getX(),
+			engine->getHero()->getY() - actor->getY()
+		);
 	}
 	return updated;
 }
 
+void BehaviourMonster::_executeMove(rRpg *engine, Actor *actor, const int x, const int y) {
+	bool executed = false;
+	Command *command = 0;
+	if (y < 0) {
+		command = new MoveUpCommand();
+	}
+	else if (y > 0) {
+		command = new MoveDownCommand();
+	}
+
+	if (command != 0) {
+		executed = command->execute(actor, engine->getMap());
+		delete command;
+		command = 0;
+	}
+
+	if (!executed) {
+		if (x < 0) {
+			command = new MoveLeftCommand();
+		}
+		else if (x > 0) {
+			command = new MoveRightCommand();
+		}
+	}
+
+	if (command != 0) {
+		executed = command->execute(actor, engine->getMap());
+		delete command;
+	}
+}
