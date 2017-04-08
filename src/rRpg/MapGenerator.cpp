cb7605175d743a72ecdff0ac64304599af1f128f
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-04-08 04:40:17 +0100

    rocks, soil and start point set randomly

--- src/rRpg/MapGenerator.cpp
+++ src/rRpg/MapGenerator.cpp
@@ -8,11 +8,6 @@ Map MapGenerator::generate(E_MapType type, short width, short height) {
 	Map map;
 	map.setDimensions(width, height);
 	map.setDisplayTileDimensions(16, 16);
-	map.setStartPoint(
-		(float) (rand() % width),
-		(float) (rand() % height)
-	);
-
 
 	if (type == CAVE) {
 		_generateCave(map);
@@ -23,11 +18,23 @@ Map MapGenerator::generate(E_MapType type, short width, short height) {
 
 void MapGenerator::_generateCave(Map &map) {
 	map.initializeGrid(TERRAIN_ROCK_NORMAL_CENTER);
-	int startPointX = (int) map.getStartPoint().getX(),
-		startPointY = (int) map.getStartPoint().getY();
-	map.setTile(startPointX, startPointY, TERRAIN_SOIL_NORMAL_CENTER);
-	map.setTile(startPointX + 1, startPointY, TERRAIN_SOIL_NORMAL_CENTER);
-	map.setTile(startPointX - 1, startPointY, TERRAIN_SOIL_NORMAL_CENTER);
-	map.setTile(startPointX, startPointY + 1, TERRAIN_SOIL_NORMAL_CENTER);
-	map.setTile(startPointX, startPointY - 1, TERRAIN_SOIL_NORMAL_CENTER);
+	unsigned int mapWidth = map.getWidth(),
+		mapHeight = map.getHeight();
+	bool startPointSet = 0;
+	for (unsigned int j = 0; j < mapHeight; ++j) {
+		for (unsigned int i = 0; i < mapWidth; ++i) {
+			E_TerrainType type;
+			if (rand() % 100 > 30) {
+				type = TERRAIN_SOIL_NORMAL_CENTER;
+				if (!startPointSet && (rand() % 100) > 90) {
+					startPointSet = 1;
+					map.setStartPoint((float) i, (float) j);
+				}
+			}
+			else {
+				type = TERRAIN_ROCK_NORMAL_CENTER;
+			}
+			map.setTile(i, j, type);
+		}
+	}
 }
