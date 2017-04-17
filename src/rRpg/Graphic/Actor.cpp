99c99146c3f19fa3c17624bbfe3ae7f460e3c99d
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-04-17 16:26:57 +0100

    game and texture renderer stored in the Graphic class

--- src/rRpg/Graphic/Actor.cpp
+++ src/rRpg/Graphic/Actor.cpp
@@ -1,20 +1,38 @@
 #include "Actor.hpp"
 #include "../Actor.hpp"
 #include "../ActorRace.hpp"
-#include "SDL2_framework/Game.h"
-#include "SDL2_framework/TextureManager.h"
 
 const int TILE_WIDTH = 16;
 const int TILE_HEIGHT = 16;
 
+GraphicActor::GraphicActor() {
+	m_textureManager = TextureManager::Instance();
+	m_game = Game::Instance();
+}
+
+GraphicActor::GraphicActor(const GraphicActor &r) :
+	m_textureManager(r.m_textureManager),
+	m_game(r.m_game)
+{
+}
+
+GraphicActor & GraphicActor::operator=(const GraphicActor &r) {
+	// check for "self assignment" and do nothing in that case
+	if (this == &r) {
+		return *this;
+	}
+
+	m_textureManager = r.m_textureManager;
+	m_game = r.m_game;
+	return *this;
+}
+
 GraphicActor::~GraphicActor() {}
 
 void GraphicActor::render(unsigned int displayShiftX, unsigned int displayShiftY, Actor *actor) {
-	TextureManager *manager = TextureManager::Instance();
-	Game *game = Game::Instance();
 	unsigned int xScreen = actor->getX() * TILE_WIDTH + displayShiftX;
 	unsigned int yScreen = actor->getY() * TILE_HEIGHT + displayShiftY;
-	manager->drawTile(
+	m_textureManager->drawTile(
 		actor->getRace().getTilesetName(),
 		0, // margin
 		0, // spacing
@@ -24,7 +42,7 @@ void GraphicActor::render(unsigned int displayShiftX, unsigned int displayShiftY
 		TILE_HEIGHT,
 		(int) actor->getRace().getSpriteY() + 1,
 		(int) actor->getRace().getSpriteX(),
-		game->getRenderer()
+		m_game->getRenderer()
 	);
 
 	// render HPs
@@ -33,6 +51,6 @@ void GraphicActor::render(unsigned int displayShiftX, unsigned int displayShiftY
 	r.y = yScreen;
 	r.w = TILE_WIDTH * actor->getHealth() / actor->getMaxHealth();
 	r.h = 2;
-	SDL_SetRenderDrawColor(game->getRenderer(), 0xff, 0, 0, 255);
-	SDL_RenderFillRect(game->getRenderer(), &r);
+	SDL_SetRenderDrawColor(m_game->getRenderer(), 0xff, 0, 0, 255);
+	SDL_RenderFillRect(m_game->getRenderer(), &r);
 }
--- src/rRpg/Graphic/Actor.hpp
+++ src/rRpg/Graphic/Actor.hpp
@@ -2,11 +2,20 @@
 #define __GRAPHIC_ACTOR__
 
 #include "Graphic.hpp"
+#include "SDL2_framework/Game.h"
+#include "SDL2_framework/TextureManager.h"
 
 class Actor;
 
 class GraphicActor : public Graphic {
+	protected:
+	TextureManager *m_textureManager = 0;
+	Game *m_game = 0;
+
 	public:
+	GraphicActor();
+	GraphicActor(const GraphicActor &L); // copy constructor
+	GraphicActor & operator=(const GraphicActor &L); // assignment
 	~GraphicActor();
 	void render(
 		unsigned int displayShiftX,
