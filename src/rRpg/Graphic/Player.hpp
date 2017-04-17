9f02646b93eda9db16c4b541d1c28215f9d5cad6
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-04-17 16:40:21 +0100

    The health of the player is now drawn at the bottom of the screen

--- src/rRpg/Graphic/Player.hpp
+++ src/rRpg/Graphic/Player.hpp
@@ -4,6 +4,8 @@
 #include "Actor.hpp"
 
 class GraphicPlayer : public GraphicActor {
+	protected:
+	void _renderHP(unsigned int displayShiftX, unsigned int displayShiftY, Actor *actor);
 };
 
 #endif
