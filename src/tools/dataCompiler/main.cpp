5c47828ae5712b3f612016dacb34bba27c372bdb
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-05-22 22:22:24 +0100

    Parser/Tile does not exist anymore

--- src/tools/dataCompiler/main.cpp
+++ src/tools/dataCompiler/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <unistd.h>
 #include <libgen.h>
-#include "Parser/Tile.hpp"
+#include "types.hpp"
 #include "ResourceManager.hpp"
 
 std::string cleanFileInPath(std::string path);
