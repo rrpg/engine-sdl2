52e6234f60291ddb065ccf27cf06b30c7a1ccec9
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-07-06 09:51:03 +0100

    circular field of view set

--- src/rRpg/FieldOfView.cpp
+++ src/rRpg/FieldOfView.cpp
@@ -3,12 +3,16 @@
 #include "Map.hpp"
 
 #define PLAYER_DEPTH_OF_VIEW 12
+#define SQ_PLAYER_DEPTH_OF_VIEW 144
 
 void FieldOfView::calculate(Map &map, std::shared_ptr<Actor> reference) {
 	m_vVisibleCells.clear();
 	for (int x = reference->getX() - PLAYER_DEPTH_OF_VIEW; x < reference->getX() + PLAYER_DEPTH_OF_VIEW; ++x) {
 		for (int y = reference->getY() - PLAYER_DEPTH_OF_VIEW; y < reference->getY() + PLAYER_DEPTH_OF_VIEW; ++y) {
-			if (map.areCoordinatesValid(x, y)) {
+			int distanceX = reference->getX() - x,
+				distanceY = reference->getY() - y,
+				distance = distanceX * distanceX + distanceY * distanceY;
+			if (distance <= SQ_PLAYER_DEPTH_OF_VIEW && map.areCoordinatesValid(x, y)) {
 				m_vVisibleCells.push_back({x, y});
 			}
 		}
