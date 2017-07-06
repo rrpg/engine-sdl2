07652e93135a466acd9964c85852c3d10e4868d8
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-07-06 09:50:45 +0100

    value not hard coded anymore

--- src/rRpg/FieldOfView.cpp
+++ src/rRpg/FieldOfView.cpp
@@ -2,10 +2,12 @@
 #include "Actor.hpp"
 #include "Map.hpp"
 
+#define PLAYER_DEPTH_OF_VIEW 12
+
 void FieldOfView::calculate(Map &map, std::shared_ptr<Actor> reference) {
 	m_vVisibleCells.clear();
-	for (int x = reference->getX() - 8; x < reference->getX() + 8; ++x) {
-		for (int y = reference->getY() - 8; y < reference->getY() + 8; ++y) {
+	for (int x = reference->getX() - PLAYER_DEPTH_OF_VIEW; x < reference->getX() + PLAYER_DEPTH_OF_VIEW; ++x) {
+		for (int y = reference->getY() - PLAYER_DEPTH_OF_VIEW; y < reference->getY() + PLAYER_DEPTH_OF_VIEW; ++y) {
 			if (map.areCoordinatesValid(x, y)) {
 				m_vVisibleCells.push_back({x, y});
 			}
