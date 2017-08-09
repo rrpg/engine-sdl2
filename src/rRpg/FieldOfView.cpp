018764d49185fce2853a158d7adbbf4ffeb34e45
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-08-09 13:39:21 +0100

    indent

--- src/rRpg/FieldOfView.cpp
+++ src/rRpg/FieldOfView.cpp
@@ -60,62 +60,62 @@ void FieldOfView::_lightQuadrant(
 ) {
 	int x = reference->getX(),
 		y = reference->getY();
-    if (startSlope < endSlope) {
-        return;
-    }
-    double nextStartSlope = startSlope;
-    for (int i = row; i <= PLAYER_DEPTH_OF_VIEW; i++) {
-        bool blocked = false;
-        for (int dx = -i, dy = -i; dx <= 0; dx++) {
-            double lSlope = (dx - 0.5) / (dy + 0.5);
-            double rSlope = (dx + 0.5) / (dy - 0.5);
-            if (startSlope < rSlope) {
-                continue;
-            } else if (endSlope > lSlope) {
-                break;
-            }
+	if (startSlope < endSlope) {
+		return;
+	}
+	double nextStartSlope = startSlope;
+	for (int i = row; i <= PLAYER_DEPTH_OF_VIEW; i++) {
+		bool blocked = false;
+		for (int dx = -i, dy = -i; dx <= 0; dx++) {
+			double lSlope = (dx - 0.5) / (dy + 0.5);
+			double rSlope = (dx + 0.5) / (dy - 0.5);
+			if (startSlope < rSlope) {
+				continue;
+			} else if (endSlope > lSlope) {
+				break;
+			}
 
-            int sax = dx * xx + dy * xy;
-            int say = dx * yx + dy * yy;
-            if ((sax < 0 && (int)std::abs(sax) > x)
-                || (say < 0 && (int)std::abs(say) > y)) {
-                continue;
-            }
-            int ax = x + sax;
-            int ay = y + say;
-            if (!map.areCoordinatesValid(ax, ay)) {
-                continue;
-            }
+			int sax = dx * xx + dy * xy;
+			int say = dx * yx + dy * yy;
+			if ((sax < 0 && (int)std::abs(sax) > x)
+				|| (say < 0 && (int)std::abs(say) > y)) {
+				continue;
+			}
+			int ax = x + sax;
+			int ay = y + say;
+			if (!map.areCoordinatesValid(ax, ay)) {
+				continue;
+			}
 
 #ifdef __DEBUG__
 			_setCellVisible(ax, ay);
 			continue;
 #endif
 
-            int radius2 = SQ_PLAYER_DEPTH_OF_VIEW;
-            if ((int) (dx * dx + dy * dy) < radius2) {
+			int radius2 = SQ_PLAYER_DEPTH_OF_VIEW;
+			if ((int) (dx * dx + dy * dy) < radius2) {
 				_setCellVisible(ax, ay);
-            }
+			}
 
-            if (blocked) {
-                if (map.isCellObstructingView(ax, ay)) {
-                    nextStartSlope = rSlope;
-                    continue;
-                } else {
-                    blocked = false;
-                    startSlope = nextStartSlope;
-                }
-            } else if (map.isCellObstructingView(ax, ay)) {
-                blocked = true;
-                nextStartSlope = rSlope;
-                _lightQuadrant(map, reference, i + 1, startSlope, lSlope, xx,
-                        xy, yx, yy);
-            }
-        }
-        if (blocked) {
-            break;
-        }
-    }
+			if (blocked) {
+				if (map.isCellObstructingView(ax, ay)) {
+					nextStartSlope = rSlope;
+					continue;
+				} else {
+					blocked = false;
+					startSlope = nextStartSlope;
+				}
+			} else if (map.isCellObstructingView(ax, ay)) {
+				blocked = true;
+				nextStartSlope = rSlope;
+				_lightQuadrant(map, reference, i + 1, startSlope, lSlope, xx,
+						xy, yx, yy);
+			}
+		}
+		if (blocked) {
+			break;
+		}
+	}
 }
 
 std::vector<std::pair<t_coordinates, char>> FieldOfView::getVisibleCells() {
